b72a1ac4c0623071d1f4d4b675ab88068ab8d475
no0ddy <naveen.rawat75@gmail.com>
2022-06-16 18:43:57 +0530

    again

--- src/util/database.js
+++ src/util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("klub_works", "root", "N@veen2911", {
   dialect: "mysql",
   host: "localhost",
+  port: 3306,
 });
 
 module.exports = sequelize;
